61b0587194fffc268082b9e09845f351e5e07301
makenamaszer <makenamaszer@raes-mbp.wireless-1x.unc.edu>
2019-08-26 22:46:41 -0400

    test

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -3,12 +3,45 @@ package a1;
 import java.util.Scanner;
 
 public class A1Novice {
-
+	
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
+		
+		// Read in number of customers
+		// int count = scan.nextInt();
+		
+		// Create an array to store customer info.
+		
+		 /*int[] custInfo = new int[count];
+		
+		
+		for (int i = 0; i < count; i++) {
+				
+			
+		}
+		*/
+		
+		double total = 0;
+		
+		String first = scan.nextLine();
+		String last = scan.nextLine();
+		int numItems = scan.nextInt();
+		
+		
+		for (int i = 0; i < numItems; i++) {
+			scan.nextInt();
+			scan.nextLine();
+			double cost = scan.nextInt();
+			total = total + cost;
+		}
+		
+		// All input parsed, so close scanner
+		scan.close();
+		
+		System.out.println(first.charAt(0) + ". " + last + ": " + total);
 		
 	}
 }
