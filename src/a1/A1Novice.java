14ad02f819af833e317c844d6aa580d0b9834f83
makenamaszer <makenamaszer@raes-mbp.wireless-1x.unc.edu>
2019-08-27 21:08:15 -0400

    xxx

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -1,8 +1,16 @@
 package a1;
 
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class A1Novice {
+	
+	String first;
+	
+	public A1Novice() {
+		first = null;
+	}
+	
 	
 	public static void main(String[] args) {
 		
@@ -11,37 +19,37 @@ public class A1Novice {
 		// Your code follows here.
 		
 		// Read in number of customers
-		// int count = scan.nextInt();
-		
-		// Create an array to store customer info.
-		
-		 /*int[] custInfo = new int[count];
+		int count = scan.nextInt();
 		
 		
-		for (int i = 0; i < count; i++) {
+		/*for (int i = 0; i < count; i++) {
 				
 			
 		}
 		*/
+				
+		String first = scan.next();
+		String last = scan.next();
+		
+		int numItems = scan.nextInt();
 		
 		double total = 0;
 		
-		String first = scan.nextLine();
-		String last = scan.nextLine();
-		int numItems = scan.nextInt();
-		
-		
-		for (int i = 0; i < numItems; i++) {
-			scan.nextInt();
-			scan.nextLine();
-			double cost = scan.nextInt();
+		for  (int i = 0; i < numItems; i++) {
+			int quantity = scan.nextInt();
+			String food = scan.next();
+			double cost = scan.nextDouble();
+			
+			cost = quantity * cost;
+			
 			total = total + cost;
 		}
 		
 		// All input parsed, so close scanner
 		scan.close();
-		
+			
+		// Print output
 		System.out.println(first.charAt(0) + ". " + last + ": " + total);
-		
+
 	}
 }
